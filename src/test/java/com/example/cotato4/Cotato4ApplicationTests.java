73ae97d360e187424472267d8ff10bccfde4c8b2
morenow <jjang9877@gmail.com>
2023-08-25 15:54:44 +0900

    first commit

---
+++ src/test/java/com/example/cotato4/Cotato4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cotato4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cotato4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
