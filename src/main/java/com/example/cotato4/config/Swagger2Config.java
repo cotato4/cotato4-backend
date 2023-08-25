50e0b15afd9158b1d1e0f734fe47891c100622dd
morenow <jjang9877@gmail.com>
2023-08-25 16:27:51 +0900

    Chore: 버전 및 설정 다수 변경

---
+++ src/main/java/com/example/cotato4/config/Swagger2Config.java
@@ -0,0 +1,25 @@
+package com.example.cotato4.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Swagger2Config {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("cotato hackathon group 4")
+                .pathsToMatch("/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("cotato4 BackEnd")
+                        .description("cotato4 BackEnd"));
+    }
+}
